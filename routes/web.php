a452b5c0565ad11f6bd817bbdc08f102f822602b
Ahsanullah <ahsannje@gmail.com>
2023-10-11 13:55:27 +0600

    Admin profile protected URL by Middleware

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,12 @@ require __DIR__.'/auth.php';
 // Admin Controller All Method:
 Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
 Route::get('/logout', [AdminController::class, 'AdminLogoutPage'])->name('admin.logout.page');
+
+Route::middleware(['auth'])->group(function(){
+
 Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
 Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password'); 
 Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');  
+
+});//End User Middleware
