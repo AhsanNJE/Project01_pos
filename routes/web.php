db0d1be8ba93a982c97dd23de70110d0241b1ad3
Ahsanullah <ahsannje@gmail.com>
2023-10-19 10:50:09 +0600

    Create CategoryController & Category Model then Migrate | Add Category with Modal Form | Finally Edit and Update & Delete Data

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Backend\CustomerController;
 use App\Http\Controllers\Backend\SupplierController;
 use App\Http\Controllers\Backend\SalaryController;
 use App\Http\Controllers\Backend\AttendenceController;
+use App\Http\Controllers\Backend\CategoryController;
 
 
 
@@ -110,6 +111,16 @@ Route::middleware(['auth'])->group(function(){
         Route::get('/edit/employee/attend/{date}','EditEmployeeAttendence')->name('employee.attend.edit');
         Route::get('/view/employee/attend/{date}','ViewEmployeeAttendence')->name('employee.attend.view');   
     });//End Attendence All Route 
+
+    //CategoryController All Route 
+    Route::controller(CategoryController::class)->group(function(){
+
+        Route::get('/all/category','AllCategory')->name('all.category'); 
+        Route::post('/store/category','StoreCategory')->name('category.store');
+        Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
+        Route::post('/update/category','UpdateCategory')->name('category.update'); 
+        Route::get('/delete/category/{id}','DeleteCategory')->name('delete.category');
+    });//End CategoryController All Route 
 
 
 
